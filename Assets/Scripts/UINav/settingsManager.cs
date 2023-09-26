7862b579bf3f5b07a66e359393f60914e18e5ca5
Medikenji <nolanbijmholt@gmail.com>
2023-09-26 13:46:55 +0200

    working settings panel

---
+++ Assets/Scripts/UINav/settingsManager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class settingsManagaer : MonoBehaviour{
+    public void switcher()
+    {
+        Console.WriteLine(gameObject.activeSelf);
+    }
+}
