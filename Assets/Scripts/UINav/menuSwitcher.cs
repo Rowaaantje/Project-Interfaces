338e04d88078e0e84959e03d0dde205decec2705
Medikenji <nolanbijmholt@gmail.com>
2023-10-09 13:29:14 +0200

    Menu fix
    Het werkt tenzij je het actief probeert te breken maar dat kan denk ik niet met goed geimplementeerde controls

--- Assets/Scripts/UINav/menuSwitcher.cs
+++ Assets/Scripts/UINav/menuSwitcher.cs
@@ -8,10 +8,22 @@ public class menuSwitcher : MonoBehaviour
     public GameObject Camera;
     public int newPosX;
     public int newPosY;
+    private bool Activated = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Vector3 newPos = new Vector3(newPosX, newPosY, -150);
-        Camera.transform.position = newPos;
-        Debug.Log("moved to new pos");
+        if (Activated == false)
+        {
+            Vector3 newPos = new Vector3(newPosX, newPosY, -730);
+            Camera.transform.position = newPos;
+            Debug.Log("moved to new pos");
+            Activated = true;
+        }
+        else
+        {
+            Vector3 newPos = new Vector3(0, 0, -730);
+            Camera.transform.position = newPos;
+            Debug.Log("moved to new pos");
+            Activated = false;
+        }
     }
 }
