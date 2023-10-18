4d219fa77edb5153f0f9347978fff0931fe0a370
Tim <t.vanhijum@student.alfa-college.nl>
2023-10-18 12:30:50 +0200

    Gun.cs + gun.prefab

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Gun : MonoBehaviour
 {
+    public float gunrange;
 
+    public Transform XYZ_enemy;
+
+    public bool inrange;
     
     // Start is called before the first frame update
     void Start()
@@ -16,5 +21,13 @@ public class Gun : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit attackHit;
+
+        Ray attack = new Ray(transform.position, transform.forward * gunrange);
+        Debug.DrawRay(transform.position, transform.forward * gunrange, Color.red);
     }
 }
