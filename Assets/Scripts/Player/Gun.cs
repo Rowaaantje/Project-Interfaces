2ac49b55fb02487c1b0cc0354b67250bad201186
Tim <t.vanhijum@student.alfa-college.nl>
2023-10-19 13:22:33 +0200

    GameSceneForMe_SoThereWontBeConflicts
    gun.cs

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -1,47 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEditor.UI;
 using UnityEngine;
 
 
 public class Gun : MonoBehaviour
 {
-    public float gunrange;
+    public Transform gun;
+    public float gunRotationSpeed = 100f;
 
-    public bool inrange;
-    
+    private GameObject enemytag;
+    public bool targetSpotted = false;
+
+    public Transform shootingPoint;
+    public GameObject bulletPrefab;
+
+    bool FIRE = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemy spotted");
+            enemytag = GameObject.FindWithTag("Enemy");
+            targetSpotted = true;
+        }
+        else
+        {
+            targetSpotted = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemy Lost");
+            RotateToNormal();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void RotateToNormal()
+    {
+        if(transform.rotation.z > 0)
+        {
+            transform.Rotate(0, 0, -gunRotationSpeed * Time.deltaTime);
+        }
+        if(transform.rotation.z < 0)
+        {
+            transform.Rotate(0, 0, gunRotationSpeed * Time.deltaTime);
+        }
+    }
+    /*void RotateTowardsEnemy()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(enemytag.transform.position - transform.position), gunRotationSpeed * Time.deltaTime);
+    }*/
+
     // Update is called once per frame
     void Update()
     {
-        RaycastHit attackHit;
-
-        Ray attack = new Ray(transform.position, transform.forward * gunrange);
-        Debug.DrawRay(transform.position, transform.forward * gunrange, Color.red);
-
-        if (Physics.Raycast(attack, out attackHit, gunrange))
+        if(targetSpotted)
         {
-            if (attackHit.collider.gameObject.tag == "Enemy")
-            {
-                inrange = true;
-            }
-            else
-            {
-                inrange = false;
-            }
-        }
-
-        /*if (inrange)
-        {
-            transform.LookAt(target.transform);
+            transform.right = enemytag.transform.position - transform.position;
+            //RotateTowardsEnemy();
         }
         else
         {
-            transform.Rotate(0, 0, 0);
-        }*/
+            RotateToNormal();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, shootingPoint.position, transform.rotation);
     }
 }
