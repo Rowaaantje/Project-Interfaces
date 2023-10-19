666ea05a6adaa15ecf2c5b8dfe8f1ca01c126182
Medikenji <nolanbijmholt@gmail.com>
2023-10-19 10:01:01 +0200

    Comments bij playertest.cs

--- Assets/Scripts/Player/PlayerTest.cs
+++ Assets/Scripts/Player/PlayerTest.cs
@@ -23,18 +23,23 @@ public class PlayerTest : MonoBehaviour
 
     public void Energy(int energy, GameObject range)
     {
+        /*Check if parameter energy is equal to EnergyLevel to turn on the range collider*/
        range.SetActive(EnergyLevel == energy);
     }
     public void Game_math()
     {
+        /*Rotate with the combined speed of _rightThrust and _leftThrust*/
         _rotationSpeed = _rightThrust + (_leftThrust * -1);
         Ship.rotation += _rotationSpeed * Time.deltaTime;
+        /*If _leftThrust + _rightThrust are more then 0 use formula ((_leftThrust + RightThrust)^1/3)*0.0015 to gain an exponent thrust.
+         If _leftThrust + _rightThrust are less then 0 inverse the formula to gain backwards thrust at a slower rate*/
         if (_leftThrust + _rightThrust > 0)
         {
             Thrust = (((float)math.pow(x: _leftThrust + _rightThrust, y: Math.Sqrt(3))) * 0.0015f) * ThrustSpeed;
         }
         else
-        {
+        { 
+            /*Inverse the formula*/
             var inverse = -1 * (_leftThrust + _rightThrust);
             Thrust = ((-1 * ((float)math.pow(x: inverse, y: Math.Sqrt(3)))) * 0.001f) * ThrustSpeed;
         }
@@ -42,6 +47,7 @@ public class PlayerTest : MonoBehaviour
     }
     public void Controller()
     {
+        /*Check if controller or keyboard is used (for testing) to only allow the Game_math() function to run with right inputs*/
         if (Input.GetAxis("Left") != 0f || Input.GetAxis("Right") != 0f)
         {
             _leftThrust = Input.GetAxis("Left") * MaxRotation;
@@ -57,7 +63,6 @@ public class PlayerTest : MonoBehaviour
             if (Input.GetKey(KeyCode.D) && _rightThrust > -MaxRotation)
             {
                 _rightThrust -= RotateThrustSpeed;
-
             }
             if (Input.GetKey(KeyCode.W) && _leftThrust < MaxRotation)
             {
@@ -73,6 +78,7 @@ public class PlayerTest : MonoBehaviour
 
     public void Energy_manager()
     {
+        /*Activate RangeColliders by using the Energy() function*/
         if (Input.GetKeyDown(KeyCode.UpArrow) && EnergyLevel < 3)
         {
             EnergyLevel += 1;
